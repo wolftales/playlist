19be7677ce51d132ed380f8a2783bc8bc5d1fc62
Ken Hillier <36087111+wolftales@users.noreply.github.com>
2023-06-18 03:24:58 +0000

    Initial Commit

---
+++ dev-notes.md
@@ -0,0 +1,17 @@
+# Capture YouTube Playlist
+---
+
+Reading a *YouTube* playlist and creating a list of artists & songs.  
+
+
+## Code Architecture
+---
+
+How I envision I may start
+
+1. Input: *YouTube* playlist 
+2. Webscrape PLaylist info form *YouTube*
+3. Manipulate data format
+4. Output: File - default
+5. (Optional) Output: File
+
---
+++ playlist.py
@@ -0,0 +1,33 @@
+from time import time
+from pytube import YouTube, Playlist
+from concurrent.futures import ThreadPoolExecutor, as_completed
+
+def get_playlist():
+    """
+    Asks user for playlist title
+    """
+
+    
+
+playlist_link = "https://www.youtube.com/playlist?list=PLJKfZ_cKGyLdYqdzGLCJPbsi9UGCcEc5e"
+video_links = Playlist(playlist_link).video_urls
+start = time()
+
+
+def get_video_title(link):
+    title = YouTube(link).title
+    return title
+
+
+processes = []
+with ThreadPoolExecutor(max_workers=10) as executor:
+    for url in video_links:
+        processes.append(executor.submit(get_video_title, url))
+
+video_titles = []
+for task in as_completed(processes):
+    video_titles.append(task.result())
+    print(task.result())
+
+
+print(f'Time taken: {time() - start}')
